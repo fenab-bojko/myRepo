2187bb469126ec1cb6c6fbfc3f05b5190c2fe993
fenab-bojko <fenab.bojko@yandex.ru>
2023-11-15 22:26:39 +0300

    carousel

--- lessone_17/script.js
+++ lessone_17/script.js
@@ -6,19 +6,23 @@ const $containerCarousel = document.querySelector('.container-carousel')
 
 const $indicatorCollection = document.querySelectorAll('.indicator')
 
-let widthItem = 640
+let widthItem = $containerCarousel.offsetWidth
 
 let itemColectionLength = $itemCollection.length
 
+let loop = 1
+
 let translate = 0
 
+let indexIndicator = 0
 
 const handlerClickButton = (e) => {
-    // console.dir(e.target)
 
     if (e.target.classList.contains('prev')) {
 
         if(!translate) {
+
+            if (!loop) return
 
             translate = -(itemColectionLength - 1) * widthItem
 
@@ -32,7 +36,10 @@ const handlerClickButton = (e) => {
 
         if(translate === -(itemColectionLength - 1) * widthItem ) {
 
+            if (!loop) return
+
             translate = 0
+
         } else {
 
             translate -= widthItem
@@ -43,54 +50,45 @@ const handlerClickButton = (e) => {
 
    $containerCarousel.style.transform='translateX(' + translate + 'px)'
 
-   indicatorActive()
+   indexIndicator = Math.abs(translate / widthItem)
+
+   indicatorActive(indexIndicator)
 
 }
 
 const handlerClickIndicator = (event) => {
 
-//    console.log(event.target.dataset.index) 
     switch (event.target.dataset.index){
 
         case '0': translate = 0
         break
 
-        case '1': translate =  -640
+        case '1': translate =  -(widthItem)
         break
 
-        case '2': translate= -1280
+        case '2': translate= -(widthItem * 2)
         break   
     }
 
     $containerCarousel.style.transform='translateX(' + translate + 'px)'
+
+    indicatorActive(+event.target.dataset.index)
+
+}
+
+const indicatorActive = (indexIndicator) => {
     
-    indicatorActive()
+    $indicatorCollection.forEach((e, i) => {
 
+        e.className = 'indicator'
+
+        i === indexIndicator ? e.classList.add('indicator-active') : ''
+
+    })
+  
 }
 
-const indicatorActive = () => {
-    for (i=0; i<itemColectionLength; i++) {
-
-        $indicatorCollection[i].classList.remove('indicator-active')
-
-    }
-
-    if (translate === 0) {
-
-        $indicatorCollection[0].classList.add('indicator-active')
-
-    } else if (translate === -640) {
-
-        $indicatorCollection[1].classList.add('indicator-active')
-
-    } else if (translate === -1280) {
-
-        $indicatorCollection[2].classList.add('indicator-active')
-
-    }
-}
-
-indicatorActive()
+indicatorActive(indexIndicator)
 
 $buttonSlideCollection.forEach((e) => {
 
