725fd6df4ac02ad82ae85a2ed581083f9ef0b59e
fenab-bojko <fenab.bojko@yandex.ru>
2023-11-18 00:07:12 +0300

    Modal Logic

--- lessone_19/landing-example/js/script.js
+++ lessone_19/landing-example/js/script.js
@@ -1,68 +1,125 @@
-const baseUsers = {
-    user1: {
-
+const USER = {
     email: 'ivan@gmail.com',
-
     password: '&$9yst_32hd',
-
-    }
 }
 
-
-
-const checkUser = (email, password) => {
-    const userEmail = document.querySelector('#userEmail').value 
-    const userPassword = document.querySelector('#userPassword').value
-    console.log(userEmail)
-    console.log(userPassword)
-    console.log('enter')
-    if (baseUsers.user1.email === email && baseUsers.user1.password === password) {
-        $fade.className = 'fade hidden'
-    }
-
-}
+let result = 0
 
 const $fade = document.querySelector('.fade')
-const $modal = document.getElementById('modal')
+const $button = document.querySelectorAll('.button')
+const $containerInfo = document.querySelector('#modal-autorisacion')
+const $bankCard = document.querySelector('#modal-bank')
+const $input = document.querySelectorAll('.modal .container-input input')
+const $headerButton = document.querySelectorAll('.header .button')
+const $avatar = document.querySelector('.header .avatar')
+const $hint = document.querySelectorAll('.container-input .hint')
 
-const $buttonLogIn = document.querySelectorAll('.button')
+const openAutorisation = () => {
 
-const $fade = document.querySelector('.fade')
-
-const clickLogIn = (elem) => {
-    switch(elem.target.dataset.name) {
-        case 'log_in': $fade.className = 'fade'
-        break
-        case 'cancel': $fade.className = 'fade hidden'
-        break
-        case 'enter': checkUser(userEmail, userPassword)
-    }
-    // if (elem.target.dataset.name === 'log_in') {
-    //     $fade.className = 'fade'
-    // }
+    $fade.classList.remove('hidden')
+    $containerInfo.classList.remove('hidden')
 
 }
 
+const openBankCard = () => {
+
+    $fade.classList.remove('hidden')
+    $bankCard.classList.remove('hidden')
+
+}
+
+const closeModal = () => {
+
+    $fade.classList.add ('hidden')
+    $bankCard.classList.add ('hidden')
+    $containerInfo.classList.add ('hidden')
+
+    $input.forEach ((e) => {
+        e.classList.remove('error')
+    })
+
+    $hint.forEach ((e) => {
+        e.classList.add('hidden')
+    })
+
+    
+
+}
+
+const isError = () => {
+    
+    $input.forEach ((e) => {
+        e.classList.add('error')
+    })
+
+    $hint.forEach ((e) => {
+        e.classList.remove('hidden')
+    })
+
+}
+
+const isLegal = () => {
+
+    $input.forEach ((e) => {
+        e.classList.add('error')
+    })
+
+    $hint.forEach ((e) => {
+        e.classList.add('hidden')
+    })
 
 
-$buttonLogIn.forEach((e) => {
+    $headerButton.forEach ((e) => {
+        e.classList.add('hidden')
+    })
 
-    e.addEventListener('click', clickLogIn)
+    $avatar.classList.remove('hidden')
+
+    closeModal()
+}
+
+const getAutorisacion = () => {
+    
+    let emailUs = document.querySelector ('#email').value
+    let passwordUs = document.querySelector ('#pass').value
+    
+    if (USER.email === emailUs &&  USER.password === passwordUs) {
+
+        isLegal()
+
+    } else {
+
+        isError()
+
+    }
+}
+
+const getLissener = (e) => {
+    console.dir(e.target)
+
+    let but = e.target.dataset.name
+
+    switch (but){
+        case 'start': openBankCard()
+        break
+        case 'log_in': openAutorisation()
+        break
+        case 'cancel': closeModal()
+        break
+        case 'enter': getAutorisacion()
+        break
+    }
+    
+} 
+
+$fade.addEventListener('click', (e) => {
+
+    e.target === e.currentTarget ? closeModal() : ''
+})
+
+$button.forEach((e) => {
+
+    e.addEventListener('click', getLissener)
 
 })
 
-$fade.addEventListener('click',() => {
-
-    if (e.target.classList.contain('fade')) {
-        $fade.className = 'fade hidden'
-    }
-})
-
-
-const createModal = () => {
-
-
-
-}
-
-createModal()
